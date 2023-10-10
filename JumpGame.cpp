5c564a03359754be13c5511e3d2ca83ff8e13eee
Pasiooo <102529903+Pasiooo@users.noreply.github.com>
2023-10-10 20:52:13 +0200

    Add files via upload

---
+++ JumpGame.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <vector>
+
+class Solution {
+public:
+    bool canJump(std::vector<int>& nums) {
+        int v_size = nums.size();
+        if (v_size < 2){
+            return true;
+        }
+        return jump_(nums, 0, 0, v_size);
+    }
+private:
+    bool jump_(std::vector<int>& nums, int current_idx, int distance, int v_size){
+        if (current_idx >= v_size){
+            return false;
+        }
+        if (current_idx == v_size - 1){
+            return true;
+        }
+        distance = nums[current_idx];
+        if (distance == 0){
+            return false;
+        }
+        int max_distance = distance;
+        for (int i = max_distance; i > 0; i--){
+            bool zmienna = jump_(nums, current_idx + i, distance, v_size);
+            if (zmienna){
+                return true;
+            }
+        }
+        return false;
+    }
+};
+
+int main(){
+    std::vector<int> wektor = {1, 2, 4, 2, 3, 0, 1, 2, 0, 1};
+    Solution wynik;
+    std::cout << wynik.canJump(wektor);
+}
---
+++ Palindrome.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <vector>
+#include <cmath>
+
+class PalindromeCheck {
+public:
+    bool isPalindrome(int x) {
+        if (x < 0){
+            return false;
+        }
+        std::vector<int> cyfry;
+        int counter = 0;
+        while (true){
+            if (x < pow(10, counter)){
+                counter--;
+                break;
+            } 
+            counter++;
+        }
+        for (int i = counter; i >= 0; i--){
+            int cyfra = x / pow(10, i);
+            cyfry.push_back(cyfra);
+            x -= cyfra * pow(10, i);
+        }
+        for (int i = 0; i < cyfry.size() / 2; i ++){
+            if (cyfry[i] != cyfry[cyfry.size() - 1 - i]){
+                return false;
+            }
+        }
+        return true;
+    }
+};
+
+int main(){
+    PalindromeCheck wynik;
+    std::cout << wynik.isPalindrome(1234321);
+}
